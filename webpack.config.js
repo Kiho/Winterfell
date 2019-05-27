612f22716a15becaa96aef50fa24df2deb3406e4
Kiho <kihoc@gmail.com>
2019-05-26 23:12:14 -0700

    Upgrade to babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,39 @@
-const webpack = require('webpack');
-const path    = require('path');
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
-  context   : __dirname + '/src',
-  entry     : {
-    javascript : './index.js'
+  context: __dirname + "/src",
+  entry: {
+    javascript: "./index.js"
   },
-  module    : {
-    loaders : [
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"]
+      },
       {
         test: /\.css$/,
-        loaders: [
-          'style-loader', 'css-loader', 'resolve-url-loader'
-        ],
-      },      
-      {
-        test    : /\.js$/,
-        exclude : /node_modules/,
-        loaders : ['babel-loader'],
-      }
-    ],
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      },
+    ]
   },
-  externals : {
-    'react'        : 'React',
-    'react/addons' : 'React'
+  externals: {
+    react: "React",
+    "react/addons": "React"
   },
-  output    : {
-    libraryTarget : 'var',
-    library       : 'Winterfell',
-    filename      : 'winterfell.min.js',
-    path          : __dirname + '/dist'
-  },
-  plugins  : [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress : {
-        warnings : false
-      }
-    })
-  ]
+  output: {
+    libraryTarget: "var",
+    library: "Winterfell",
+    filename: "winterfell.min.js",
+    path: __dirname + "/dist"
+  }
 };
